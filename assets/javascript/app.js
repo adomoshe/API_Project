20f1b711246078505757dd028cd2b6407192998a
Ado <Ado@ados-macbook.lan>
2018-11-18 21:41:18 -0800

    Added comments to JS file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,11 @@ $(window).ready(function () {
     var gifPlayArr = [];
     var gifStillArr = [];
 
-
+    /*This notation enables buttons that are dynamically created to be registered. 
+    When a .movieBtn is pressed spaces in its data-name are converted to '+' and an ajax GET call is made asking for 10 gifs.
+    After the call is completed  a for loop pushes the still url and the rating into respective arrays.
+    The rating is inserted into an h4 tag and prepended to the #gifInsert div
+    Then an image with the fixed height movie url is prepended to the #gifInsert div*/
     $(document).on("click", ".movieBtn", function () {
         apiCallValue = ($(this).data("name").replace(/ /g, "+"));
         queryURL = "https://api.giphy.com/v1/gifs/search?q=" + apiCallValue + "&api_key=1wQx6yxcOuJcqqriBDVG9HTuZ8LdlhWi&limit=10";
@@ -20,11 +24,13 @@ $(window).ready(function () {
                 $("#gifInsert").prepend('<h4> Rating: ' + (rating[rating.length - 1]) + '</4>')
                 $("#gifInsert").prepend('<img id=' + (gifStillArr.length - 1) + ' src=' + gifStillArr[(gifStillArr.length - 1)] + '>');
                 gifPlayArr.push(response.data[i].images.fixed_height.url);
-
             };
         });
     });
 
+    /* Whenever an image is clicked an if statement fires off checking the src url against url in the gifStillArr in the index stored in the id of the image
+    (the id was previously set as the last index of the array). If this is found to be true then the src url is switched to the same index but from the gifPlayArr.
+    Else the src url must be a gifPlayArr url so it is changed back to a still on click.*/
     $(document).on("click", "img", function () {
         if ($(this).attr("src") == gifStillArr[this.id]) {
             $(this).attr("src", function () {
@@ -38,6 +44,9 @@ $(window).ready(function () {
     });
 
 
+    /* if the #submitButton is pressed then the value from the form is extracted, trimmed of white spaces and bound as a variable to newButtonValue.
+    newButton is constructed with several bootstrap classes and a data-name and caption that are what the user inputed.
+    The button is then appended to the #buttonInsert div and the #submitForm is finally cleared.*/
     $("#submitButton").on("click", function (enter) {
         enter.preventDefault();
         var newButtonValue = $("#submitForm").val().trim();
